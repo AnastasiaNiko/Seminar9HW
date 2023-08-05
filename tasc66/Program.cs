de10459d1fff1556af865d460013af86e767aab4
Анастасия Николаева <nikolaeva@MacBook-Pro-Anastasia.local>
2023-08-06 01:46:56 +0300

    tasc66

---
+++ tasc66/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задайте значение M и N. Напишите программу, которая найдет
+//сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+
+int m = Number("Введите значение M: ");
+int n = Number("Введите значение N: ");
+Console.WriteLine($"M={m}; N={n} -> {NatNumber(m, n)}");
+
+int Number(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NatNumber(int m, int n)
+{
+    if(m == n) return n;
+    return n + NatNumber(m, n - 1);   
+}
